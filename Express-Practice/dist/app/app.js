239ffa142d4bea96b163514c618e96d5849e46b8
Hemel Bourhan <hemelbourhan@gmailgit config --global user.email Bourhan>
2023-11-23 16:00:44 +0600

    global error handling

--- Express-Practice/dist/app/app.js
+++ Express-Practice/dist/app/app.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -13,7 +22,7 @@ app.use(express_1.default.text());
 const userRouter = express_1.default.Router();
 const courseRouter = express_1.default.Router();
 app.use("/api/v1/users", userRouter);
-app.use('/api/v1/courses', courseRouter);
+app.use("/api/v1/courses", courseRouter);
 userRouter.post("/create-user", (req, res) => {
     const user = req.body;
     console.log(user);
@@ -23,12 +32,12 @@ userRouter.post("/create-user", (req, res) => {
         data: user,
     });
 });
-courseRouter.post('/create-course', (req, res) => {
+courseRouter.post("/create-course", (req, res) => {
     const course = req.body;
     console.log(course);
     res.json({
         success: true,
-        message: 'user is created successfully',
+        message: "user is created successfully",
         data: course,
     });
 });
@@ -40,14 +49,37 @@ const logger = (req, res, next) => {
 //   console.log(req.params);
 //   res.send("Hello MERN Full-Stack Developers !");
 // });
-app.get("/", logger, (req, res) => {
-    console.log(req.query);
-    res.send("Hello MERN Full-Stack Developers !");
-});
-app.post("/", logger, (req, res) => {
-    console.log(req.body);
-    res.json({
-        message: "successfully receive data ",
-    });
+// app.get("/", logger, (req: Request, res: Response) => {
+//   console.log(req.query);
+//   res.send("Hello MERN Full-Stack Developers !");
+// });
+// app.post("/", logger, (req: Request, res: Response) => {
+//   console.log(req.body);
+//   res.json({
+//     message: "successfully receive data ",
+//   });
+// });
+app.get("/", logger, (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        res.send(something);
+    }
+    catch (error) {
+        console.log(error);
+        next(error);
+        // res.status(400).json({
+        //   success: false,
+        //   message: "failed to get data ",
+        // });
+    }
+}));
+// global error handler
+app.use((error, req, res, next) => {
+    console.log(error);
+    if (error) {
+        res.status(400).json({
+            success: false,
+            message: " failed to data "
+        });
+    }
 });
 exports.default = app;
--- Express-Practice/src/app/app.ts
+++ Express-Practice/src/app/app.ts
@@ -8,10 +8,10 @@ app.use(express.text());
 
 // router
 const userRouter = express.Router();
-const courseRouter = express.Router()
+const courseRouter = express.Router();
 
 app.use("/api/v1/users", userRouter);
-app.use('/api/v1/courses', courseRouter)
+app.use("/api/v1/courses", courseRouter);
 
 userRouter.post("/create-user", (req: Request, res: Response) => {
   const user = req.body;
@@ -24,16 +24,16 @@ userRouter.post("/create-user", (req: Request, res: Response) => {
   });
 });
 
-courseRouter.post('/create-course', (req: Request , res : Response)=>{
+courseRouter.post("/create-course", (req: Request, res: Response) => {
   const course = req.body;
   console.log(course);
 
   res.json({
-    success : true,
-    message : 'user is created successfully',
-    data : course,
-  })
-})
+    success: true,
+    message: "user is created successfully",
+    data: course,
+  });
+});
 
 const logger = (req: Request, res: Response, next: NextFunction) => {
   console.log(req.url, req.method, req.hostname);
@@ -44,15 +44,43 @@ const logger = (req: Request, res: Response, next: NextFunction) => {
 //   console.log(req.params);
 //   res.send("Hello MERN Full-Stack Developers !");
 // });
-app.get("/", logger, (req: Request, res: Response) => {
-  console.log(req.query);
-  res.send("Hello MERN Full-Stack Developers !");
-});
+// app.get("/", logger, (req: Request, res: Response) => {
+//   console.log(req.query);
+//   res.send("Hello MERN Full-Stack Developers !");
+// });
 
-app.post("/", logger, (req: Request, res: Response) => {
-  console.log(req.body);
-  res.json({
-    message: "successfully receive data ",
-  });
+// app.post("/", logger, (req: Request, res: Response) => {
+//   console.log(req.body);
+//   res.json({
+//     message: "successfully receive data ",
+//   });
+// });
+
+app.get(
+  "/",
+  logger,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      res.send(something);
+    } catch (error) {
+      console.log(error);
+      next(error);
+      // res.status(400).json({
+      //   success: false,
+      //   message: "failed to get data ",
+      // });
+    }
+  }
+);
+
+// global error handler
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  console.log(error);
+  if(error){
+    res.status(400).json({
+      success : false,
+      message : " failed to data "
+    })
+  }
 });
 export default app;
